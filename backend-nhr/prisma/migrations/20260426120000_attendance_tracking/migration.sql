be854dc74a6742f10a5fdd445668ca5fc48356b4
miladhalabi <halabimilad@gmail.com>
2026-06-13 00:01:41 +0300

    add TelegramBotService

--- backend-nhr/prisma/migrations/20260426120000_attendance_tracking/migration.sql
+++
@@ -1,4 +0,0 @@
--- Add new appointment lifecycle states for scheduled booking and attendance tracking
-ALTER TYPE "AppointmentStatus" ADD VALUE IF NOT EXISTS 'BOOKED';
-ALTER TYPE "AppointmentStatus" ADD VALUE IF NOT EXISTS 'ATTENDED';
-ALTER TYPE "AppointmentStatus" ADD VALUE IF NOT EXISTS 'NO_SHOW';
